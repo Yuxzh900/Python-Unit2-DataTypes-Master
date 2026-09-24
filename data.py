612dafa95128a12543acbdb983fe84ae9b4ba792
Yuxzh900 <yuxiangz6@nycstudents.net>
2026-09-24 06:24:22 -0700

    Unit Two (9/24//2026)

--- data.py
+++ data.py
@@ -1,4 +1,36 @@
-x = 3
-y = float(3)
-print(x,y)
+# x = 3
+# y = float(3)
+# print(x,y)
 
+# You will be creating a tip calculator that must accomplish all of the following
+
+# Create variables representing at the bill, tip and total amount paid
+# Receive user input and assign that user input to the variables in step 1 (excluding total)
+# Change the data type of bill from String to Float
+# Change the data type of tip to Integer (int)
+# Calculate the total that needs to be paid
+# Print the f string after the user has input data
+
+# def bill_calculator():
+#     print("How much was the bill?")
+#     input 
+    
+#     print(input * 14)
+# bill_calculator()
+
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(values[0])
+#     print(values[6])
+
+# x = "words are here"
+# y= x.split( )
+# z = y[0]
+# print(y)
+# print(z)
+
+input ("Setence here")
+x=input()
+y= x.split({" "})
+print(len(x))
